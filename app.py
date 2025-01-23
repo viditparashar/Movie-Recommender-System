c1b1a7584bdf5c23076d660b24589b1075691039
Vidit Parashar <88538261+viditparashar@users.noreply.github.com>
2025-01-23 13:05:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,27 @@
 import pickle
 import streamlit as st
-import requests
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=cbfbede8757e308fbae9dd4297f6cac2&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
 
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
+    try:
+        index = movies[movies['title'] == movie].index[0]
+        distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+        recommended_movie_names = []
 
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+        for i in distances[1:6]:  
+            recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names,recommended_movie_posters
+        return recommended_movie_names
+    except IndexError:
+        return ["Movie not found in the dataset!"]
 
 
-st.header('Movie Recommender System Using Machine Learning')
-movies = pickle.load(open('movies.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
+
+st.header('🎥 Movie Recommender System')
+
+
+movies = pickle.load(open('movies.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
@@ -35,21 +30,22 @@ selected_movie = st.selectbox(
 )
 
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+    recommended_movie_names = recommend(selected_movie)
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    if recommended_movie_names:
+        
+        st.markdown("#### Recommended Movies:")
+        st.markdown(
+            f"""
+            <div style="display: flex; justify-content: flex-start; gap: 70px;">
+                <div style="text-align: center; font-size: 18px; font-weight: bold;">{recommended_movie_names[0]}</div>
+                <div style="text-align: center; font-size: 18px; font-weight: bold;">{recommended_movie_names[1]}</div>
+                <div style="text-align: center; font-size: 18px; font-weight: bold;">{recommended_movie_names[2]}</div>
+                <div style="text-align: center; font-size: 18px; font-weight: bold;">{recommended_movie_names[3]}</div>
+                <div style="text-align: center; font-size: 18px; font-weight: bold;">{recommended_movie_names[4]}</div>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+    else:
+        st.error("No recommendations found!")
